f4654b1d2741ecbe094f123fcd2de67c3a98c766
Matthew Koch <matthew.z.koch@gmail.com>
2015-09-14 21:34:22 -0400

    Created JUnit test file for ConstrainedMedian with the given Udacity test cases.

---
+++ ConstrainedMedianTest.java
@@ -0,0 +1,46 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+public class ConstrainedMedianTest {
+
+	@Test
+	public void test1() {
+		int n = 3;
+		int[] xPresent = {0,1};
+		int[] yPresent = {0,1};
+		int xMissing = 1;
+		int yMissing = 0;
+		int k = 1;
+		
+		ConstrainedMedian cm = new ConstrainedMedian(n, xPresent, yPresent, xMissing, yMissing);
+		assertEquals(1, cm.minimizeDistance(k));
+	}
+
+	@Test
+	public void test2() {
+		int n = 5;
+		int[] xPresent = {1,2,3,4};
+		int[] yPresent = {0,1,2,3};
+		int xMissing = 0;
+		int yMissing = 4;
+		int k = 0;
+		
+		ConstrainedMedian cm = new ConstrainedMedian(n, xPresent, yPresent, xMissing, yMissing);
+		assertEquals(0, cm.minimizeDistance(k));
+	}
+
+	@Test
+	public void test3() {
+		int n = 7;
+		int[] xPresent = {30,41,5,11,2,19};
+		int[] yPresent = {15,32,39,6,12,21};
+		int xMissing = 0;
+		int yMissing = 5;
+		int k = 10;
+		
+		ConstrainedMedian cm = new ConstrainedMedian(n, xPresent, yPresent, xMissing, yMissing);
+		assertEquals(19, cm.minimizeDistance(k));
+	}
+
+}
