65c5d9e1ceebd5d58abedc3687be8200533c9e92
Matthew Koch <matthew.z.koch@gmail.com>
2015-09-14 21:43:06 -0400

    Added test case 3 which was skipped in the first commit.

--- ConstrainedMedianTest.java
+++ ConstrainedMedianTest.java
@@ -32,6 +32,19 @@ public class ConstrainedMedianTest {
 
 	@Test
 	public void test3() {
+		int n = 5;
+		int[] xPresent = {1,2,3,4};
+		int[] yPresent = {0,1,2,3};
+		int xMissing = 4;
+		int yMissing = 0;
+		int k = 2;
+		
+		ConstrainedMedian cm = new ConstrainedMedian(n, xPresent, yPresent, xMissing, yMissing);
+		assertEquals(2, cm.minimizeDistance(k));
+	}
+
+	@Test
+	public void test4() {
 		int n = 7;
 		int[] xPresent = {30,41,5,11,2,19};
 		int[] yPresent = {15,32,39,6,12,21};
