163234fd07db9742cef7e758c710dd3f716d280a
Michael Simpson <mjs2600@gmail.com>
2015-09-16 07:43:17 -0400

    Test maximum constraints outlined in @190

--- ConstrainedMedianTest.java
+++ ConstrainedMedianTest.java
@@ -63,5 +63,17 @@ public class ConstrainedMedianTest {
 		ConstrainedMedian cm = new ConstrainedMedian(n, xPresent, yPresent, xMissing, yMissing);
 		assertEquals(19, cm.minimizeDistance(k));
 	}
-
+	
+	@Test
+        public void test5() {
+	        int n = 21;
+	        int[] xPresent = {-256, 256, 23, 12, -199, -63, 32, 187, 199, -72, 20, 44, 39, 26, 144, 135, 244, -167, -33, 12};
+	        int[] yPresent = {15, 32, 39, 6, 12, 21, 256, -256, 24, -87, -92, -233, -196, -85, 123, 12, 135, 243, -222, 19 };
+	        int xMissing = 15;
+	        int yMissing = 5;
+	        int k = 8;
+	
+	        ConstrainedMedian cm = new ConstrainedMedian(n, xPresent, yPresent, xMissing, yMissing);
+	        assertEquals(455, cm.minimizeDistance(k));
+    	}
 }
